5a17b98210c6ea04fe06743c5bc503af4b3af268
zerpajose <zjose88@gmail.com>
2025-10-24 23:30:36 -0300

    fix: include file extension in generated file name for upload

--- src/helpers/upload-file.js
+++ src/helpers/upload-file.js
@@ -3,8 +3,8 @@ import { storage } from '../services/storage.js';
 
 export async function uploadFileToBucket(userId, file) {
   const { bucketName } = config;
-  const fileName = `${userId}-${Date.now()}`;
   const extension = file.originalname.split('.').pop().toLowerCase();
+  const fileName = `${userId}-${Date.now()}.${extension}`;
   await storage.bucket(bucketName).file(fileName).save(file.buffer);
-  return `https://storage.googleapis.com/${bucketName}/${fileName}.${extension}`;
+  return `https://storage.googleapis.com/${bucketName}/${fileName}`;
 }
